7361d67bf7af8bb7f190b55eabae7a2c8e46d796
Adam <94191657+pepsiman3@users.noreply.github.com>
2023-12-03 13:51:25 -0700

    finalize presentation PID values

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,24 @@
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define SERVO_PIN    9
 
-#define PING_INTERVAL 50
+#define PING_INTERVAL 15
 
 #define MAX_DISTANCE 30 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-#define MEDIAN_SAMPLES 15
-#define OUTLIER_THRESHOLD 300
-#define OUTLIER_REJECT_CYCLES 5 
+#define MEDIAN_SAMPLES 10
 
 int samples[MEDIAN_SAMPLES]{};
+int diff_samples[MEDIAN_SAMPLES - 1]{};
+
 int sorted_samples[MEDIAN_SAMPLES]{};
 
 // Define vars we'll be connecting to
 double Setpoint, Input, Output;
 
-const double Akp = 1.2, Aki = 1, Akd = 1;
+const double Akp = -0.0641, Aki = -0.0013, Akd = -0.6467;
 const double Ckp = 0.2, Cki = 1, Ckd = 0.15;
 
-double Kp = 1.1, Ki = 1, Kd = 0.85;
+double Kp = 0.03144, Ki = 0.01752, Kd = 0.02;
+//double Kp = 0, Ki = 0, Kd = 0.05;
 
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
@@ -42,6 +43,26 @@ int outlier_ctr = 0;
 long last_time = 0;
 long curr_time = 0;
 
+void prune_outliers(){
+
+  for(int i = 0; i < MEDIAN_SAMPLES - 1; ++i){
+    diff_samples[i] = samples[i+1] - samples[i];
+  }
+
+  float stddev = std_deviation(diff_samples, MEDIAN_SAMPLES - 1);
+  float avg = get_avg(diff_samples, MEDIAN_SAMPLES - 1);
+
+  for(int i = 1; i < MEDIAN_SAMPLES - 1; ++i){
+    if((float)diff_samples[i] >= avg + stddev || (float)diff_samples[i] <= avg - stddev){
+      diff_samples[i] = diff_samples[i-1];
+    }
+    samples[i] = samples[i-1] + diff_samples[i-1];
+    samples[i] = constrain(samples[i],0,MAX_DISTANCE);
+  }
+  samples[MEDIAN_SAMPLES - 1] = samples[MEDIAN_SAMPLES - 2] + diff_samples[MEDIAN_SAMPLES - 2];
+  samples[MEDIAN_SAMPLES - 1] = constrain(samples[MEDIAN_SAMPLES - 1],0,MAX_DISTANCE);
+}
+
 void setup() {
   Serial.begin(9600);
   Input = sonar.ping_cm();
@@ -49,15 +70,16 @@ void setup() {
 
   myServo.attach(SERVO_PIN);
   myServo.write(45); delay(3000);
-  Setpoint = 15;
+  Setpoint = 9;
 
-  myPID.SetOutputLimits(-45, 45);
+  myPID.SetOutputLimits(-3.142, 3.142);
+  myPID.SetDerivativeFilterSamples(10);
 
   myPID.SetMode(AUTOMATIC);
-  myPID.SetSampleTime(100);
+  myPID.SetSampleTime(15);
 }
 
-void loop() {
+void loop() { 
   curr_time = millis();
 
   last_dist = dist;
@@ -69,34 +91,31 @@ void loop() {
     //Serial.println(dist);
 
     if(dist > 27 && dist < 30){dist = 27;}
-    /*if(abs(dist-last_dist) >= OUTLIER_THRESHOLD || outlier_ctr != 0) 
-      {dist = last_dist + ((float)(dist-last_dist) * 0.1);
-      outlier_ctr++;
-      if(outlier_ctr >= OUTLIER_REJECT_CYCLES){outlier_ctr = 0;}
-    }*/
-    //if(dist == 0) add_sample(samples[MEDIAN_SAMPLES - 1], samples, MEDIAN_SAMPLES);
-    add_sample(dist, samples, MEDIAN_SAMPLES);
 
+    add_sample(dist, samples, MEDIAN_SAMPLES);
+    outlier_ctr++;
+
+    if(outlier_ctr >= MEDIAN_SAMPLES){
+      //prune_outliers();
+      outlier_ctr = 0;
+    }
     for(int i = 0; i < MEDIAN_SAMPLES; i++){
       sorted_samples[i] = samples[i];
     }
-
   }
   
   Input = get_median(sorted_samples, MEDIAN_SAMPLES);
   
-  if(abs(Input - Setpoint) <= 4){
+  /*if(abs(Input - Setpoint) <= 4){
     Kp = Ckp; Ki = Cki; Kd = Ckd;
   }else{
     Kp = Akp; Ki = Aki; Kd = Akd;
-  }
+  }*/
 
   if(abs(Input - Setpoint) <= 1){
     Input = Setpoint;
   }
   myPID.Compute();
-
-  myServo.write((int)((float)85 - (float)Output));
-
-  Serial.print("Input:\t"); Serial.print(Input); Serial.print(" Output:\t"); Serial.println(Output);
+  myServo.write(92-constrain(degrees(Output),-45,45));
+  Serial.print(Input); Serial.print(" "); Serial.println(degrees(Output));
 }
