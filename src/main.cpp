53770b64422ce6176d9ecf85c57ec2d68b258f8c
Adam <94191657+pepsiman3@users.noreply.github.com>
2023-12-03 22:54:52 -0700

    add potentiometer functionality

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,15 @@
 #define MAX_DISTANCE 30 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 #define MEDIAN_SAMPLES 10
 
+#define POTENTIOMETER_SAMPLES 20
+
 int samples[MEDIAN_SAMPLES]{};
 int diff_samples[MEDIAN_SAMPLES - 1]{};
 
 int sorted_samples[MEDIAN_SAMPLES]{};
+
+int pot_samples[POTENTIOMETER_SAMPLES];
+
 
 // Define vars we'll be connecting to
 double Setpoint, Input, Output;
@@ -27,7 +32,7 @@ double Setpoint, Input, Output;
 const double Akp = -0.0641, Aki = -0.0013, Akd = -0.6467;
 const double Ckp = 0.2, Cki = 1, Ckd = 0.15;
 
-double Kp = 0.03144, Ki = 0.01752, Kd = 0.02;
+double Kp = 0.03144, Ki = 0.0252, Kd = 0.03;
 //double Kp = 0, Ki = 0, Kd = 0.05;
 
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
@@ -69,13 +74,21 @@ void setup() {
   dist = Input; last_dist = Input;
 
   myServo.attach(SERVO_PIN);
-  myServo.write(45); delay(3000);
-  Setpoint = 9;
+  myServo.write(70); delay(2000);
 
   myPID.SetOutputLimits(-3.142, 3.142);
-  myPID.SetDerivativeFilterSamples(10);
+  myPID.SetDerivativeFilterSamples(15);
 
   myPID.SetMode(AUTOMATIC);
+
+  for(int i = 0; i < POTENTIOMETER_SAMPLES; ++i){
+    int pot_sample = (int)(analogRead(A1)/1023.f*18.f + 7.f);
+
+    add_sample(constrain(pot_sample,7,25), pot_samples, POTENTIOMETER_SAMPLES);
+    Setpoint = (int)get_avg(pot_samples, POTENTIOMETER_SAMPLES);
+    delay(30);
+  }
+
   myPID.SetSampleTime(15);
 }
 
@@ -84,7 +97,13 @@ void loop() {
 
   last_dist = dist;
   
-  if(curr_time - last_time >=  PING_INTERVAL){ 
+  if(curr_time - last_time >=  PING_INTERVAL){
+    Serial.print("s");Serial.print(Setpoint);Serial.print("i"); Serial.print(Input);Serial.print("o");Serial.println((int)degrees(Output));
+    int pot_sample = (int)(analogRead(A1)/1023.f*18.f + 7.f);
+
+    add_sample(constrain(pot_sample,7,25), pot_samples, POTENTIOMETER_SAMPLES);
+    Setpoint = (int)get_avg(pot_samples, POTENTIOMETER_SAMPLES);
+
     last_time = curr_time;  
     dist = sonar.ping_cm();
 
@@ -116,6 +135,6 @@ void loop() {
     Input = Setpoint;
   }
   myPID.Compute();
-  myServo.write(92-constrain(degrees(Output),-45,45));
-  Serial.print(Input); Serial.print(" "); Serial.println(degrees(Output));
+  int angle = constrain(degrees(Output),-45,45);
+  myServo.write(92-angle);
 }
